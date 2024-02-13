c74262f721993478293773ccf2ff14377bf38de2
faouzisalma <faouzisalma123123@gmail.com>
2024-02-13 15:23:57 +0100

    add scanner to class

--- Desktop/java folder/helloworld/src/HelloWorld.java
+++ Desktop/java folder/helloworld/src/HelloWorld.java
@@ -1,9 +1,14 @@
-public class HelloWorld {
-	// Your First Program
+import java.util.Scanner;
 
-	    public static void main(String[] args) {
-	        System.out.println("Hello, World!");
-		System.out.println("Hello, World!");
-	    
-	}
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    // Java Scanner String input example
+    System.out.println("What is your name?");
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.nextLine();
+    System.out.println(name + " is a nice name!");
+    System.out.println("Hello, World!");
+
+  }
 }
